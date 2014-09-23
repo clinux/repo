0ab6f2fe2f06d38da1734dd1dc40ccccfa021457
Jianh Li <clinux@outlook.com>
2014-09-23 21:54:02 +0800

    Create interactive.input.output.cpp

---
+++ interactive.input.output.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	while (true)
+	{
+		while (cin >> num)
+		{
+			cout << num;
+		}
+
+		if (cin.bad())
+		{
+			break;
+		}
+
+		if (cin.eof())
+		{
+			break;
+		}
+
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<stream_size>::max(), '\n');
+			continue;
+		}
+	}
+
+	return 0;
+}
